cd94c40d913548dd7a6863ad7394df2c6cf9d7b2
Dominic Fraser <mrdjfraser@gmail.com>
2018-03-25 00:31:55 +0000

    made sqlite database, added functions to database utilities

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,8 +1,29 @@
+import sqlite3 as lite
+
+
 def create_database(database_path: str):
-    # TODO: generate the database
-    pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("drop table if exists words")
+        ddl = "create table words" \
+              " (word TEXT not null primary key, usage_count INT not null);" \
+              " create unique index words_word_uindex on words (word);"
+        conn.close()
 
 
 def save_words_to_database(database_path: str, words_list: list):
-    # TODO: save the words to the database
-    pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        for word in words_list:
+            sql = "select count(word) from words where word='" + word + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "update words set usage_count = usage_count + 1 where word = '" + word + "'"
+            else:
+                sql = "insert into words(word) values ('" + word + "')"
+            cur.execute(sql)
+            conn.close()
+            print("Database save complete!")
